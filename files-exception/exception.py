02ecccf2a70b5fd13d1c20d6346aa250d1dce921
nickyzinn16 <rickydavidbans27@gmail.com>
2026-05-04 17:43:12 -0100

    Segundo commit

---
+++ files-exception/exception.py
@@ -0,0 +1,12 @@
+try: 
+    num1 = int(input("Introzuda primeiro numero: "))
+    num2 = int(input("Introzuda Segundo numero: "))
+
+    result = num1/num2
+    print(f"Resultado: {result}!")
+
+except ZeroDivisionError:
+    print(f"Erro: Divisao com 0!")
+
+except ValueError:
+    print(f"Erro: Carater Invalido!")
